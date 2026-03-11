d1008809df7fbce22538b4abf9ca75349f2cffb5
kiraxd7 <sagar74822@gmail.com>
2026-03-11 06:05:03 +0000

    exposed 443 port of the gateway container for ssl

--- scripts/aws/start_gateway.sh
+++ scripts/aws/start_gateway.sh
@@ -4,7 +4,7 @@ if [ "$(docker ps -aq -f name=nginx-gateway)" ]; then
     docker stop nginx-gateway && docker rm nginx-gateway
 fi
 
-docker run -d -p 80:80 --name nginx-gateway \
+docker run -d -p 80:80 -p 443:443 --name nginx-gateway \
   -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro \
   -v /etc/letsencrypt:/etc/letsencrypt:ro \
   nginx:latest
